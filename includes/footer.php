97ccf10be9f487e6ba001fa74a758020ff01b9a5
Phong Van <danhpro4411@gmail.com>
2023-05-12 18:20:46 +0700

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,113 @@
+<footer id="footer" class="footer color-bg">
+    <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class="contact-info">
+                        <div class="footer-logo">
+                            <div class="logo">
+                                <a href="index.php">
+
+                                    <h3>ShopHIEUTHUHAI</h3>
+                                </a>
+                            </div><!-- /.logo -->
+
+                        </div><!-- /.footer-logo -->
+
+                        <div class="module-body m-t-20">
+                            <p class="about-us"> Món ăn vặt là những món ăn bình dân không còn xa lạ đối với các bạn trẻ ngày nay. Các món ăn vặt đều có hương vị đặc biệt riêng, kích thích vị giác người thưởng thức.</p>
+
+                            <div class="social-icons">
+
+                                <a href="http://facebook.com/transvelo" class='active'><i
+                                        class="icon fa fa-facebook"></i></a>
+                                <a href="#"><i class="icon fa fa-twitter"></i></a>
+                                <a href="#"><i class="icon fa fa-linkedin"></i></a>
+                                <a href="#"><i class="icon fa fa-rss"></i></a>
+                                <a href="#"><i class="icon fa fa-pinterest"></i></a>
+
+                            </div><!-- /.social-icons -->
+                        </div><!-- /.module-body -->
+
+                    </div><!-- /.contact-info -->
+                </div><!-- /.col -->
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class="contact-timing">
+                        <div class="module-heading">
+                            <h4 class="module-title">Thời gian mở cửa</h4>
+                        </div><!-- /.module-heading -->
+
+                        <div class="module-body outer-top-xs">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <tbody>
+                                        <tr>
+                                            <td>thứ 2 - thứ 6:</td>
+                                            <td class="pull-right">8h đến 18h</td>
+                                        </tr>
+                                        <tr>
+                                            <td>thứ 7:</td>
+                                            <td class="pull-right">9h đến 20h</td>
+                                        </tr>
+                                        <tr>
+                                            <td>chủ nhật:</td>
+                                            <td class="pull-right">10h đến 20h</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div><!-- /.table-responsive -->
+                        </div><!-- /.module-body -->
+                    </div><!-- /.contact-timing -->
+              </div><!-- /.col -->
+
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                 <div class="contact-information">
+                        <div class="module-heading">
+                            <h4 class="module-title">Thông tin</h4>
+                        </div><!-- /.module-heading -->
+
+                        <div class="module-body outer-top-xs">
+                            <ul class="toggle-footer" style="">
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p>Đà Nẵng, vn</p>
+                                    </div>
+                                </li>
+
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <p>(+84) 1235488965<br>(+84) 1235488965</p>
+                                    </div>
+                                </li>
+
+                                <li class="media">
+                                    <div class="pull-left">
+                                        <span class="icon fa-stack fa-lg">
+                                            <i class="fa fa-circle fa-stack-2x"></i>
+                                            <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                                        </span>
+                                    </div>
+                                    <div class="media-body">
+                                        <span><a href="#">duc@gmail.com</a></span>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div><!-- /.module-body -->
+                    </div><!-- /.contact-timing -->
+                </div>
+            </div>
+        </div>
---
+++ includes/myaccount-sidebar.php
@@ -0,0 +1,19 @@
+	<div class="col-md-4">
+<div class="checkout-progress-sidebar ">
+	<div class="panel-group">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+		    	<h4 class="unicase-checkout-title">Tiến độ thanh toán</h4>
+		    </div>
+		    <div class="panel-body">
+				<ul class="nav nav-checkout-progress list-unstyled">
+					<li><a href="my-account.php">tài khoản của tôi</a></li>
+					<li><a href="bill-ship-addresses.php">Địa chỉ giao hàng / thanh toán</a></li>
+					<li><a href="order-history.php">lịch sử đơn hàng</a></li>
+					<li><a href="pending-orders.php">chờ thanh toán</a></li>
+				</ul>		
+			</div>
+		</div>
+	</div>
+</div> 
+</div>
